a520bad759f6afcd47f38cbb6f9ecd6b563e5b12
Sherrydon4996 <edwinnjogu4996@gmail.com>
2026-02-27 17:39:55 +0300

    updated the domains

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://www.harrytechservices.com",
+      "https://tenants-smartrent.com",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
